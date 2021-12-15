c83a67b2457a3df50c9a33f3c6a0904f626e2170
ibrahimashraf18 <96183597+ibrahimashraf18@users.noreply.github.com>
2021-12-15 18:32:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,299 @@
+            //  Chp No : 21 - 25
+
+                //  Task No :1 
+
+// var firstName = prompt();
+// var lastName = prompt();
+// var fullName = firstName + lastName;
+
+// alert("Congrulations" + " " + fullName);
+
+
+                        //   Task No :2 
+
+// var phone = prompt();
+// var length = phone.length;
+
+// alert ("My favourite phone is :" + length);
+
+
+                    
+                        //   Task No :3 
+
+// var country = "pakistani";
+//  var index = country.indexOf("n"); 
+// alert("String :" + country + " " + "Index of 'n' :" +  index ) ;
+
+
+                                //    Task No :4 
+// var word = "Hello World";
+// var index = word.lastIndexOf("l"); 
+//  alert("String :" + word + " " + "Index of 'n' :" +  index ) ;
+
+
+                    //   Task No : 5
+// var country = "pakistani";
+// country = country.charAt(3);
+// console.log(country);
+
+
+
+                   //   Task No : 6
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+// var fullName = firstName + lastName;
+// alert("Congrulations" + " " + fullName);
+
+
+                   //   Task No : 7
+//  var cities = prompt();                
+// var city = "hyder";
+// if ("hyderabad === city"){
+//     console.log("Islamabad")
+// }
+
+                   //   Task No : 8
+// var a = "and";
+// var b = "&";
+// var message = "Ali" + a + "sara are best friends. They play cricket and football together";
+// if (and === a){
+//     alert(message === b);
+// }
+
+
+                   //   Task No : 9
+// var value = prompt();                   
+// var numberOne = "472";
+// if (value === numberOne){
+//     alert("number");
+// }
+
+
+                   //   Task No : 10
+// var input = "peanuts";
+// input = input.toLowerCase();
+// if ("peanuts" ===  input.toLowerCase()){
+//     alert(input.toUpperCase());
+// }
+
+
+                   //   Task No : 11
+// var user = "javascript";
+// var firstChara = user.slice(0,1);
+// firstChara = firstChara.toUpperCase();
+// var otherChara = user.slice(1);
+
+// var mixing = firstChara + otherChara;
+// alert(mixing);
+
+                   //   Task No : 12
+// var number = "35.36";
+// var change = number.splice(2,3);
+// alert(change);
+
+                   //   Task No : 14
+// var input = prompt("What do you want");                  
+//  var array = ["cake" , "cookei" , "applepie" , "chips" , "paaties"]
+
+// if (input === array[1]){
+//     alert("cookies");
+// } else {
+//     alert("Sorry we dont have this");
+// }
+
+                   //   Task No : 15
+// var password = +prompt();
+// if ("password === alphabet"){
+//   alert("It should contain alphabet and number");
+  
+// }  
+
+// else if ("password === number"){
+//     alert("It should contain number");
+  
+// }
+// else if ("password === sixDigit"){
+//     alert("It should contain six character and long");
+// }
+
+                   //   Task No : 16
+// var university = "universityofkarachi";
+// var array = ["u" ,  "n" , "i" , "v" , "e" , "r" , "s" , "i" , "t" , "y" , "o" , "f" ,
+//              , "k" , "a" , "r" , "a" , "c" , "h" , "i" ]
+
+// alert(array);
+
+                   //   Task No : 17
+// var country = "pakistan";
+// country = country.charAt(7);
+// console.log(country);
+
+                   //   
+
+
+                //    Chp No : 26 -30
+                //   Task No : 1
+// var number = 3.45214;
+// num = Math.round(3.45214)
+// num2= Math.ceil(3.45214)
+// num3 = Math.floor(3.45214)
+// console.log(num , num2 , num3);
+
+                //   Task No : 2
+// var number = -3.45214;
+// num4 = Math.round(-3.45214)
+// num5= Math.ceil(-3.45214)
+// num6 = Math.floor(-3.45214)
+// console.log(num4 , num5 , num6);
+
+                
+
+
+
+
+
+
+                //   Task No : 4
+// var randomNumber = Math.random();                
+// console.log(Math.ceil(randomNumber * 6));
+
+                //   Task No : 5
+// var head = 1;
+// var tail = 2;
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss)
+//  if (floor === 0){
+//      console.log("HEAD = 1");
+//  }
+//  else if(floor === 1){
+//      console.log("TAIL = 2");
+//  }           
+
+
+                //   Task No : 6
+// var value = Math.random();
+// var randomNumber = Math.floor(Math.random() *100);
+// alert(randomNumber);
+
+                //   Task No : 7
+// var weight = +prompt("Enter your weight in kilogram");
+// alert("The weight of user is" + " " + weight + "kgs");
+
+                //   Task No : 8
+// var number = +prompt("Enter your secret number from 1 to 10");
+// if (number === 10){
+//     alert("Congrats");
+// }
+// else if (number <10){
+//     alert("Try again");
+// }
+
+
+                        //  Chp No :31 - 34
+
+                //   Task No : 1
+// var now = new Date();
+// console.log(now);
+
+                //   Task No : 2
+// var month = user;
+// var user = prompt();
+// if (month = user){
+//     alert("December");
+// }
+
+//                 //   Task No : 3
+// var day = prompt();
+// switch (day){
+//     case "mon":
+//     alert("Monday");
+    
+//     case "tues":
+//     alert("Tuesday");
+
+//     case "wed":
+//     alert("Wednesday");
+
+//     case "thurs":
+//     alert("Thursday");
+
+//     case "fri":
+//     alert("Friday");
+
+//     case "sat":
+//     alert("Saturday");
+
+//     case "sun":
+//     alert("Sunday");
+// }
+
+
+                //   Task No : 4
+ 
+// var day = prompt();
+// switch (day){
+// case "sat":
+//     alert("Its Funday");
+
+//     case "sun":
+//     alert("Its Funday");
+// }
+
+
+                //   Task No : 5
+// var user = prompt();                
+// for (var i = 0 ; i<user.length ; i++){
+//     if(user <=15){
+//         alert("First Fifteen days")
+//     }
+//     else if (user >= 16){
+//         alert("Last second days");
+//     }
+// }
+
+                //   Task No : 7
+
+// var user = +prompt();
+
+// if (user <= 12){
+//     alert("Its PM");
+// }
+
+// else if (user >= 12){
+//     alert("Its AM");
+// }
+
+
+                //   Task No : 8
+// var currentDate = new Date();
+// currentDate.setFullYear(2021);
+// currentDate.setMonth(11);
+// currentDate.setDate(31);
+// currentDate.setHours(20);
+
+
+// console.log(currentDate);
+             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
